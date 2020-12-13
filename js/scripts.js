2c5cf3bfb630c68efdaf6b99a789abe28bbab018
dorothymuhonja <dorothymuhonja7@gmail.com>
2020-12-13 14:28:43 +0300

    add media query for container and card, make corrections to js

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,40 @@
 function generateName(date, month, yearOfBirth) {
 
+
+    var maleName = [
+      'Kwasi',
+      'Kwadwo',
+      'Kwabena',
+      'Kwaku',
+      'Yaw',
+      'Kofi',
+      'Kwame',
+    ];
+    var femaleName = [
+      'Akosu',
+      'Adwoa',
+      'Abenaa',
+      'Akua',
+      'Yaa',
+      'Afua',
+      'Amna',
+    ];
+    var dayOfWeek = [
+      'Sunday',
+      'Monday',
+      'Tuesday',
+      'Wednesday',
+      'Thursday',
+      'Friday',
+      'Saturday',
+    ];
+
     var century = yearOfBirth.slice(0, 2);
     var year = yearOfBirth.slice(2, 4);
 
     var genders = document.getElementById("gender");
     var gender = genders.options[genders.selectedIndex].text;
 
-    var maleName = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-    var femaleName = ["Akosu", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Amna"];
-    var dayOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Sunday"];
 
     var dayOfBirth = (((century / 4) - 2 * year - 1) + ((5 * year / 4)) + ((26 * (month + 1) / 10)) + date) % 7;
     var dayOfBirth = Math.floor(dayOfBirth);
